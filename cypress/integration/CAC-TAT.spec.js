761a7abe2ccaa6a70198c0ba9df6e81d2e7d3b63
willams192 <wiilhams82@gmail.com>
2024-05-23 20:37:07 -0300

    feature: Create new test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -143,5 +143,21 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.contains('Talking About Testing').should('be.visible');
     });
 
+    it.only('CN014 - Exibir e esconder as mensagens de sucesso e erro usando o .invoke', () => {
+        cy.get('.success')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Mensagem enviada com sucesso.')
+            .invoke('hide')
+            .should('not.be.visible')
+        cy.get('.error')
+            .should('not.be.visible')
+            .invoke('show')
+            .should('be.visible')
+            .and('contain', 'Valide os campos obrigatórios!')
+            .invoke('hide')
+            .should('not.be.visible')
+    })
 
 });
